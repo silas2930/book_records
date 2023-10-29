952b503984d2b09373cdf57802ce2c8b08207b53
Silas Praise Ascabano <silaspraiseascabano@gmail.com>
2023-10-29 21:40:35 +0800

    enable runtime caching

--- next.config.js
+++ next.config.js
@@ -1,21 +1,25 @@
 /** @type {import('next').NextConfig} */
+const runtimeCaching = require("next-pwa/cache");
 
-const nextConfig = {
-    async headers() {
-        return [
-          {
-            // matching all API routes
-            source: "/api/:path*",
-            headers: [
-              { key: "Access-Control-Allow-Credentials", value: "true" },
-              { key: "Access-Control-Allow-Origin", value: "*" },
-              { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
-              { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-              { key: "Cache-Control", value: "no-cache, no-store, must-revalidate" },  
-            ]
-          }
-        ]
-      }    
-  };
+const nextDataIndex = runtimeCaching.findIndex(
+  (entry) => entry.options.cacheName === "next-data"
+);
 
-module.exports = nextConfig
+if (nextDataIndex !== -1) {
+  runtimeCaching[nextDataIndex].handler = "NetworkFirst";
+} else {
+  throw new Error("Failed to find next-data object in runtime caching");
+}
+
+const withPWA = require("next-pwa")({
+  dest: "public",
+  runtimeCaching,
+  register: true,
+  skipWaiting: true,
+  buildExcludes: [/middleware-manifest.json$/],
+  disable: process.env.NODE_ENV === 'development'
+})
+
+module.exports = withPWA({
+  reactStrictMode: false
+});
