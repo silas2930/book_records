f380b0e9e90e31555ed0dcc852e9da3bf9fdf719
Silas Praise Ascabano <silaspraiseascabano@gmail.com>
2023-10-29 19:54:43 +0800

    Modify next-config js to set cache control

--- next.config.js
+++ next.config.js
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+
+const nextConfig = {
+    reactStrictMode: true,
+    async headers() {
+      return [
+        {
+          source: '/',
+          headers: [
+            {
+              key: 'Cache-Control',
+              value: 'no-cache, no-store, must-revalidate',
+            },
+          ],
+        },
+      ];
+    },
+  };
 
 module.exports = nextConfig
