4e2da0637c6609e809fb9c91571d9867451b8157
Silas Praise Ascabano <silaspraiseascabano@gmail.com>
2023-10-30 19:44:39 +0800

    patch next.config.js

--- next.config.js
+++ next.config.js
@@ -1,25 +1,8 @@
 /** @type {import('next').NextConfig} */
-const runtimeCaching = require("next-pwa/cache");
-
-const nextDataIndex = runtimeCaching.findIndex(
-  (entry) => entry.options.cacheName === "next-data"
-);
-
-if (nextDataIndex !== -1) {
-  runtimeCaching[nextDataIndex].handler = "NetworkFirst";
-} else {
-  throw new Error("Failed to find next-data object in runtime caching");
+const nextConfig = {
+  experimental: {
+      serverActions: true,
+  }
 }
 
-const withPWA = require("next-pwa")({
-  dest: "public",
-  runtimeCaching,
-  register: true,
-  skipWaiting: true,
-  buildExcludes: [/middleware-manifest.json$/],
-  disable: process.env.NODE_ENV === 'development'
-})
-
-module.exports = withPWA({
-  reactStrictMode: false
-});
+module.exports = nextConfig
