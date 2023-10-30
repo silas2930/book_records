d0e255aa7627fc0edd1935892799a214f1f374bc
Silas Praise Ascabano <silaspraiseascabano@gmail.com>
2023-10-30 21:03:53 +0800

    Revert "Testing/Debugging"
    This reverts commit a8b15f9753390cfc59808dc095ec02eb614b7005.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import PostCard from '@/components/PostCard'
+import { useState } from 'react';
 import { db } from '@/lib/db';
 import { BookOpenText } from 'lucide-react'
 
@@ -21,21 +22,20 @@ function getPosts() {
 
 export default async function Home() {
 
-  // const posts = await db.post.findMany({
-  //   select: {
-  //     id: true,
-  //     title: true,
-  //     content: true,
-  //     tag: true,
-  //   },
-  //   orderBy: {
-  //     createdAt: 'desc'
-  //   }
-  // })
-  const posts = await db.post.findMany()
-  console.log(posts)
+  const posts = await getPosts()
+  // const tags = await db.tag.findMany()
+
   return (
     <div>
+      {/* <div className="mb-4 mt-10">
+        <label className="mr-4">Display by tag:</label>
+        <select className="select select-bordered w-full max-w-sm">
+            <option value="all">All</option>
+              {tags?.map(item => (
+                  <option key={item.id} value={item.id}>{item.name}</option>
+              ))}
+        </select>
+      </div> */}
         <div className="mt-5 bg-blue-900 py-4 flex text-white items-center justify-center text-4xl font-bold">
           <BookOpenText className='mr-2' />
             Book Records
--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -8,16 +8,16 @@ interface PostCardProps{
     id: string
     title: string
     content: string
-    // tag: Tag
+    tag: Tag
   }
 }
 const PostCard: FC<PostCardProps> = ({ post }) => {
-  const { id, title, content } = post
+  const { id, title, content, tag } = post
   return (
     <div className="card w-full bg-base-100 shadow-xl">
         <div className="card-body bg-gray-900">
             <h2 className="card-title">{title}</h2>
-            {/* <span className="badge badge-neutral">{tag.name}</span> */}
+            <span className="badge badge-neutral">{tag.name}</span>
             <p className="max-h-40 overflow-hidden whitespace-nowrap overflow-ellipsis italic">{content}</p>
             <div className="card-actions justify-end">
               
